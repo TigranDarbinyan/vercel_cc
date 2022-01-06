62f7c9182e437658b5127883b9c065470b17dc60
tigran.darbinyan <tigran.darbinyan@betconstruct.com>
2022-01-06 21:57:31 +0400

    Setup the VERCEL project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <code>VERCEL</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
